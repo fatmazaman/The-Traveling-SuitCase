0e74b425620f9d379cb1d0ed6cb0e1cd0b53ede6
fatmazaman <fatma302@gmail.com>
2014-10-03 12:08:50 -0400

    stuck

--- monitor.py
+++ monitor.py
@@ -6,19 +6,18 @@ Dave_latitude = 41.98062
 
 def distance(lat1,lat2):
 	#return distance in miles between three lats
-	    return 69*abs( lat1 - lat2)
+	return 69*abs( lat1 - lat2)
+
 def monitor():
 	u = urllib.urlopen('http://www.ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
 	doc = ET.parse(u)
-	
-
-'''    for bus in doc.findall('bus'): 
+    for bus in doc.findall('bus'): 
 		busid = bus.findtext('id')
 		if busid in candidates:
 			lat = float (bus.findtext('lat'))
 			dis = distance(lat, Dave_latitude)
 			print (busid,dis,'miles')
-	print ('_'*10)'''		
+	print ('_'*10)		
 
 	
 import time 
