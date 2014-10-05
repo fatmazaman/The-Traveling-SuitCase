34db1b4c1bc71bb431f6d644ff320f23f8ef2696
fatmazaman <fatma302@gmail.com>
2014-10-04 23:28:00 -0400

    Almost got it

--- monitor.py
+++ monitor.py
@@ -1,27 +1,24 @@
-#monitor.py
 import urllib
 import xml.etree.ElementTree as ET
-candidates = ['4363','4154']
+candidates = ['4371','4163','4132']
 Dave_latitude = 41.98062
 
 def distance(lat1,lat2):
-	#return distance in miles between three lats
-	return 69*abs( lat1 - lat2)
+    #return distance in miles between three lats
+    return 69*abs(lat1 - lat2)
 
 def monitor():
-	u = urllib.urlopen('http://www.ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
-	doc = ET.parse(u)
+    u = urllib.urlopen('http://www.ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
+    doc = ET.parse(u)
     for bus in doc.findall('bus'): 
-		busid = bus.findtext('id')
-		if busid in candidates:
-			lat = float (bus.findtext('lat'))
-			dis = distance(lat, Dave_latitude)
-			print (busid,dis,'miles')
-	print ('_'*10)		
+        busid = bus.findtext('id')
+        if busid in candidates:
+            lat = float(bus.findtext('lat'))
+            dis = distance(lat, Dave_latitude)
+            print (busid,dis,'miles')
+        print ('_'*10)
 
-	
 import time 
 while True:
-	monitor()
-	time.sleep(60)
-    
+    monitor()
+    time.sleep(60)
